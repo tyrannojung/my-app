430e49a5e60b1cc4edf8f49207581b494436bac8
tyrannojung <tyranno2020@gmail.com>
2023-12-03 00:43:16 +0900

    update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 
 const handler = NextAuth({
+  secret: process.env.NEXTAUTH_URL,
   providers: [
     CredentialsProvider({
       // 이 부분은 자체 로그인 로직을 구현합니다.

e4eedcf886d84c56dce27e350155945c28fe4309
DESKTOP-929CA5E\jds00 <tyranno2020@gmail.com>
2023-12-08 20:54:16 +0900

    update

---
+++ Contracts/hardhat.config.ts
@@ -0,0 +1,33 @@
+import { HardhatUserConfig } from "hardhat/config";
+import fs from "fs";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
+
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line: string) => line.trim().split("="));
+}
+
+const config: HardhatUserConfig = {
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/".*.sol";$/)) { // match all lines with `"<any-import-path>.sol";`
+          for (const [from, to] of getRemappings()) {
+            if (line.includes(from)) {
+              line = line.replace(from, to);
+              break;
+            }
+          }
+        }
+        return line;
+      },
+    }),
+  },
+  solidity: "0.8.19",
+};
+
+export default config;
